59e83101c46befea8d6fdc87639d89c4d995f29b
Gennaro Bosone <gennaro.bs@gmail.com>
2022-02-07 23:13:16 +0100

    replace tap with jest

--- test/composeAnimation.test.js
+++ test/composeAnimation.test.js
@@ -1,38 +1,39 @@
-const tap = require('tap')
 const Animation = require('../dist/cjs')
 
-const anim = Animation.Compose([
-	{
-		from: 0,
-		to: 100,
-		duration: 1000,
-	},
-	{
-		from: 0,
-		to: 100,
-		duration: 1000,
-	},
-])
+test('compose', () => {
+	const anim = Animation.Compose([
+		{
+			from: 0,
+			to: 100,
+			duration: 1000,
+		},
+		{
+			from: 0,
+			to: 100,
+			duration: 1000,
+		},
+	])
 
-tap.equal(anim(0), 0)
-tap.equal(anim(500), 50)
-tap.equal(anim(1000), 100)
+	expect(anim(0)).toBe(0)
+	expect(anim(500)).toBe(50)
+	expect(anim(1000)).toBe(100)
 
-const anim2 = Animation.Compose([
-	{
-		from: 0,
-		to: 100,
-		duration: 1000,
-	},
-	{
-		from: 100,
-		to: 200,
-		duration: 1000,
-		delay: 500,
-		loop: true,
-	},
-])
+	const anim2 = Animation.Compose([
+		{
+			from: 0,
+			to: 100,
+			duration: 1000,
+		},
+		{
+			from: 100,
+			to: 200,
+			duration: 1000,
+			delay: 500,
+			loop: true,
+		},
+	])
 
-tap.equal(anim2(0), 50)
-tap.equal(anim2(1000), 125) // anim: 100, anim2: 150
-tap.equal(anim2(2000), 100)
+	expect(anim2(0)).toBe(50)
+	expect(anim2(1000)).toBe(125) // anim: 100, anim2: 150
+	expect(anim2(2000)).toBe(100)
+})
--- test/simpleAnimation.test.js
+++ test/simpleAnimation.test.js
@@ -1,42 +1,43 @@
-const tap = require('tap')
 const Animation = require('../dist/cjs')
 
-const anim = Animation.Simple({
-	from: 0,
-	to: 100,
-	duration: 1000,
-	loop: 2,
+test('simple animation', () => {
+	const anim = Animation.Simple({
+		from: 0,
+		to: 100,
+		duration: 1000,
+		loop: 2,
+	})
+
+	expect(anim(-10)).toBe(0)
+	expect(anim(0)).toBe(0)
+	expect(anim(500)).toBe(50)
+	expect(anim(2200)).toBe(100)
+
+	const anim2 = Animation.Simple({
+		from: 0,
+		to: 100,
+		duration: 300,
+		delay: 400,
+	})
+
+	expect(anim2(100)).toBe(0)
+	expect(anim2(550)).toBe(50)
+	expect(anim2(701)).toBe(100)
+
+	const anim3 = Animation.Loop({
+		from: 100,
+		to: 200,
+		duration: 300,
+		delay: 400,
+		afterDelay: 200,
+		direction: 'alternate',
+		interpolator: 'linear',
+	})
+
+	expect(anim3(100)).toBe(100) // not started
+	expect(anim3(550)).toBe(150) // started 400 + 150
+	expect(anim3(700)).toBe(200) // end
+	expect(anim3(800)).toBe(200) /// after delay 100
+	expect(anim3(1050)).toBe(150) // reverse anim
+	expect(anim3(400 + 300 + 200 + 300 + 400 + 150)).toBe(150) // loop 2 delay + dur + afterD + dur (reverse) + delay + dur/2
 })
-
-tap.equal(anim(-10), 0)
-tap.equal(anim(0), 0)
-tap.equal(anim(500), 50)
-tap.equal(anim(2200), 100)
-
-const anim2 = Animation.Simple({
-	from: 0,
-	to: 100,
-	duration: 300,
-	delay: 400,
-})
-
-tap.equal(anim2(100), 0)
-tap.equal(anim2(550), 50)
-tap.equal(anim2(701), 100)
-
-const anim3 = Animation.Loop({
-	from: 100,
-	to: 200,
-	duration: 300,
-	delay: 400,
-	afterDelay: 200,
-	direction: 'alternate',
-	interpolator: 'linear',
-})
-
-tap.equal(anim3(100), 100) // not started
-tap.equal(anim3(550), 150) // started 400 + 150
-tap.equal(anim3(700), 200) // end
-tap.equal(anim3(800), 200) /// after delay 100
-tap.equal(anim3(1050), 150) // reverse anim
-tap.equal(anim3(400 + 300 + 200 + 300 + 400 + 150), 150) // loop 2 delay + dur + afterD + dur (reverse) + delay + dur/2
